f7ad1c3fd0a5eff3f9dd852ba88c1508d09b5549
Daan Goslinga <agedaan@hotmail.com>
2020-09-28 11:33:48 +0200

    small fix

--- src/main/kotlin/furhatos/app/fruitseller/flow/fruitseller.kt
+++ src/main/kotlin/furhatos/app/fruitseller/flow/fruitseller.kt
@@ -249,9 +249,6 @@ val StarshipActivities = state(Interaction){
         goto(endState)
     }
     onResponse<listActivities>{
-        furhat.say("Understood. You have now successfully checked in. You will soon be teleported to your" +
-                "room, and your luggage will be delivered by our staff. We hope your stay at Starship" +
-                "Enterprise will be a fun and relaxing one.")
         goto(endState)
     }
 
