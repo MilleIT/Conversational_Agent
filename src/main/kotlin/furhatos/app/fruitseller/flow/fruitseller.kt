64081c5c824ac5d54fe219bc651c1af41747161a
sarahdewolf <s.c.m.dewolf@student.tudelft.nl>
2020-10-30 13:59:05 +0100

    randoms gefixed

--- src/main/kotlin/furhatos/app/fruitseller/flow/fruitseller.kt
+++ src/main/kotlin/furhatos/app/fruitseller/flow/fruitseller.kt
@@ -237,6 +237,7 @@ val OrderAndName = state(Interaction) {
 }
 
 val LookUpOrder = state(Interaction) {
+    val random = Random.nextInt(2) + 1;
     onEntry {
         furhat.attend(user = users.random)
         furhat.say("Thank you, I'll look up your order straight away.")
@@ -244,20 +245,15 @@ val LookUpOrder = state(Interaction) {
             goto(LookAround)
         }
         TimeUnit.SECONDS.sleep(2)
-        furhat.say {
-            +"I can see here this is about the order of a"
-            random {
-                block {
-                    +"15 inch Dell laptop"
-                    users.current.book.receivedOrder = "laptop"
-                }
-                block {
-                    +"70 inch LG Television"
-                    users.current.book.receivedOrder = "television"
-                }
-            }
-        }
 
+        if(random == 1) {
+            furhat.say("I can see here this is about the order of a 15 inch Dell laptop")
+            users.current.book.receivedOrder = "laptop"
+        }
+        else if(random == 2) {
+            furhat.say("I can see here this is about the order of a 70 inch LG Television")
+            users.current.book.receivedOrder = "television"
+        }
         parallel {
                 goto(LookQuestion)
             }
@@ -294,26 +290,20 @@ val LookForCause = state(Interaction) {
 
         furhat.attend(user = users.random)
 
-        // TODO dit is beetje warrig geprogameerd. Ene keer is random in deze val, andere keer na goto
         if(users.current.book.problem == "received the wrong package") {
             goto(WrongPackage)
         }
         else if (users.current.book.problem == "didn't receive the package") {
-            random (
-                    {goto(NotSentYet) },
-                    {goto(OnItsWay) }
-            )
-
+            val random = Random.nextInt(2) + 1;
+            if(random == 1) {goto(NotSentYet) }
+            else if(random == 2){goto(OnItsWay) }
         }
         else if (users.current.book.problem == "didn't receive a refund") {
-
-            random(
-                    {goto(RefundDelay)},
-                    {goto(RefundInMotion)},
-                    {goto(RefundNotStarted)}
-            )
+            val random = Random.nextInt(3) + 1;
+            if(random == 1){goto(RefundDelay)}
+            else if(random == 2){goto(RefundInMotion)}
+            else if(random == 3){goto(RefundNotStarted)}
         }
-
     }
 }
 
@@ -334,24 +324,18 @@ val TryOrderAgain = state(Interaction) {
 }
 
 val FoundOrder = state(Interaction) {
+    val random = Random.nextInt(2) + 1;
     onEntry {
         furhat.say ( "Ok, let me check." )
         furhat.attend(Loc())
         TimeUnit.SECONDS.sleep(3)
-        furhat.say {
-            +"Thank you for your patience, I have found your order, "
-            random {
-
-                block {
-                    +"the 13 inch HP laptop."
-                    users.current.book.receivedOrder = "laptop"
-                }
-                block {
-                    +"the 50 inch Phillips television"
-                    users.current.book.receivedOrder = "television"
-                }
-
-            }
+        if(random == 1) {
+            furhat.say("Thank you for your patience, I have found your order, the 13 inch HP laptop.")
+            users.current.book.receivedOrder = "laptop"
+        }
+        else if(random == 2) {
+            furhat.say("Thank you for your patience, I have found your order, the 50 inch Phillips television.")
+            users.current.book.receivedOrder = "television"
         }
        // TimeUnit.SECONDS.sleep(1) HEB DIT FF UITGECOMMENT, SNAP NIET WAAROM WE EEN SECONDE STILTE LATEN VALLEN
         goto(LookForCause)
@@ -759,403 +743,3 @@ val TakeInAccount = state(Interaction){
         furhat.say ( "I wish you a very nice day!" )
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*val NoInfo = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-        furhat.say("Without your information I cannot book you in. Are you sure")
-        furhat.attend(user = users.random)
-        furhat.ask("")
-
-    }
-    onResponse<Yes> {
-        furhat.say("Goodbye then")
-        goto(Idle)
-    }
-
-    onResponse<No> {
-        goto(AmountGuests)
-    }
-}
-
-val AmountGuests = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-        furhat.say("Let's get started then. How many people would you like to check-in?")
-        furhat.attend(user = users.random)
-        furhat.ask("")
-    }
-    onResponse<TellPeople> {
-        val people = it.intent.values[0].toString().toInt()
-//        furhat.say { "$people" + if (people == 1) "person" else "people" }
-//        println("${it.intent}")
-//        println("${it.intent.values[0].toString().toInt()}")
-        users.current.book.people = Number(people)
-        println("$people " + if (people == 1) "person" else "people" )
-        goto(RandomQuestion)
-    }
-}
-
-val RandomQuestion = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-        furhat.say("Great. By the way, would you like to know about the available amenities in our room?")
-        furhat.attend(user = users.random)
-        furhat.ask("")
-    }
-    onResponse<Yes> {
-        goto(RandomQuestion1Yes)
-    }
-
-    onResponse<No> {
-        println("the amount of people: " + "${users.current.book.people}")
-        goto(FurtherDetails)
-    }
-}
-
-val RandomQuestion1Yes = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-        furhat.ask("You are provided a bed, table, a chair, and a Replicator, which allows you to instantly create " +
-                "any dish you've ever wanted to eat, in the comfort of your own room. ", timeout = 0)
-
-    }
-
-    onNoResponse { goto (FurtherDetails) }
-}
-
-val FurtherDetails = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-        val name = furhat.askFor<PersonName>("Perfect. Now, could you give me your name please?")
-        furhat.attend(user = users.random)
-
-        users.current.book.name  = name?.value
-        parallel {
-            goto(LookAround)
-        }
-
-        furhat.say("Thank you. How long do you intend to stay on" +
-                "Starship Enterprise, and would you like to stay in our Suite-class rooms" +
-                "or the Citizen-class rooms? (suite class have 2 beds, citizen-class have 1 bed)")
-        furhat.attend(user = users.random)
-        furhat.ask("")
-    }
-
-    onPartialResponse<GiveLengthStay> {
-        println("Yes length partial")
-        users.current.book.lengthStay = it.intent.stayL.toString()
-        raise(it, it.secondaryIntent)
-    }
-
-    onResponse<GiveRoomClass> {
-        println("Yes room")
-        users.current.book.roomClass = it.intent.room.toString()
-        goto(Summary) // is only for testing
-        //goto(checkRooms)
-    }
-
-}
-
-val Summary = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-        furhat.say("would you like me to summarize?")
-        furhat.attend(user = users.random)
-        furhat.ask("")
-    }
-    onResponse<Yes> {
-        furhat.say ( "so let me summarize")
-        val name : String? = users.current.book.name
-        val lengthstay : String? = users.current.book.lengthStay
-        val roomClass : String? = users.current.book.roomClass
-        println("your name is ${name}, you want to stay ${lengthstay}, and you want rooms of type ${roomClass}")
-        goto(checkRooms)
-    }
-
-    onResponse<No> {
-        goto(checkRooms)
-    }
-}
-
-val checkRooms : State = state(Interaction){
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        val people : Int? = users.current.book.people?.value
-        var SuitesNeeded : Int? = 0
-        var CitroomsNeeded : Int? = people
-        val SuitesAvailable : Int? = users.current.book.suiteRooms?.value
-        val CitroomsAvailable : Int? = users.current.book.citiRooms?.value
-        if (users.current.book.roomClass == "suite") {
-            SuitesNeeded = people?.div(2) // not tested if this floors or ceilings, but it needs to ceiling
-            CitroomsNeeded = 0 // use this if line below is NOT used.
-            // CitroomsNeeded = people?.rem(2) // for uneven people and suite choice, eg 3 people = 1 suite and 1 citizen room.
-            // if line above is used, SuitesNeeded needs to floor.
-        }
-        if ((SuitesAvailable!! >= SuitesNeeded!!) && (CitroomsAvailable!! >= CitroomsNeeded!!)) {
-            users.current.book.suiteRooms = Number(SuitesAvailable - SuitesNeeded)
-            users.current.book.citiRooms = Number(CitroomsAvailable - CitroomsNeeded)
-            goto(SpecificWishes)
-        } else {
-            goto(StarshipOverloaded)
-        }
-    }
-}
-
-val StarshipOverloaded = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        val number : Number? = if (users.current.book.roomClass == "suite") users.current.book.suiteRooms else users.current.book.citiRooms
-        furhat.say("Unfortunately there are no rooms left of this kind. " +
-                "We only have $number rooms of this kind free. ")
-        furhat.attend(user = users.random)
-        furhat.ask("Would you like to change the number of people you are checking in?", timeout = 5000)
-    }
-
-    onResponse<No> {
-        goto(CheckinCancel)
-    }
-    onResponse<Yes> {
-        goto(NumberOfPeopleChange)
-    }
-    onNoResponse {
-        goto(CheckinCancel)
-    }
-}
-
-val SpecificWishes = state(Interaction){
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        val name : String? = users.current.book.name
-        furhat.say("Amazing. The data has been entered to your name, ${name}")
-        furhat.attend(user = users.random)
-        furhat.ask("Now, before asking you about the different activities we offer on board, I would like to " +
-                        "ask you if you have any specific wishes for your stay here?")
-    }
-    onResponse<No> {
-        goto(NoWishes)
-    }
-    onResponse<TellWish> {
-        goto(ExtraWish)
-    }
-}
-
-val ExtraWish = state(Interaction){
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        val random = Random.nextInt(1, 3)
-        if(random == 1 ){
-            furhat.say("Understood. Anything else?")
-            furhat.attend(user = users.random)
-            furhat.ask("")
-        }
-        else if(random == 2){
-            furhat.say("Do you have any more wishes?")
-            furhat.attend(user = users.random)
-            furhat.ask("")
-        }
-        else if(random == 3){
-            furhat.say("Is there something else you wish for?")
-            furhat.attend(user = users.random)
-            furhat.ask("")
-
-        }
-    }
-    onResponse<TellWish> {
-        reentry()
-    }
-    onResponse<No> {
-        goto(EndWishes)
-    }
-}
-
-val StarshipActivities = state(Interaction){
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        furhat.say("On Starship Enterprise we offer numerous simulated activities, namely:" +
-                "Skiing, Tennis, Badminton, and Zombie Survival. Please tell me which ones of" +
-                "those activities you would like to sign up for today.")
-        furhat.attend(user = users.random)
-        furhat.ask("")
-    }
-    onResponse<No>{
-        goto(endState)
-    }
-    onResponse<listActivities>{
-        goto(endState)
-    }
-
-}
-
-val endState = state(Interaction){
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        furhat.say("Understood. You have now successfully checked in. You will soon be teleported to your" +
-                "room, and your luggage will be delivered by our staff.")
-        furhat.attend(user = users.random)
-        furhat.say("We hope your stay at Starship Enterprise will be a fun and relaxing one.")
-
-    }
-}
-
-val CheckinCancel = state(Interaction){
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        furhat.say("Alright then, please tell me if you'd like to start over." +
-                    " Otherwise, I wish you a good day.")
-        furhat.attend(user = users.random)
-        furhat.ask("")
-    }
-    onNoResponse {
-        goto(Start)
-    }
-}
-
-val NumberOfPeopleChange = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        furhat.say("Wonderful. Please tell me how many guests you would like to check in.")
-        furhat.attend(user = users.random)
-        furhat.ask("")
-    }
-
-    onResponse<TellPeople> {
-        val people = it.intent.values[0].toString().toInt()
-        users.current.book.people = Number(people)
-        println("$people " + if (people == 1) "person" else "people" )
-        goto(FurtherDetails)
-    }
-}
-
-val Explaining = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        random(
-                {
-                    furhat.say("Welcome to StarShip Enterprise. We are currently leaving" +
-                        " for a 12-day voyage from planet Earth to planet Vulkan." +
-                        " My name is Data and I am your check-in assistant for today." +
-                        " Would you like to check in?")
-                    furhat.attend(user = users.random)
-                    furhat.ask("")
-                })
-    }
-
-    onResponse<Yes> {
-        //goto(CheckingIn) TODO dit gaf uit het niets een error
-    }
-    //onResponse<CheckIn> {
-        //goto(CheckingIn) TODO dit gaf uit het niets een error
-    //}
-}
-
-val EndWishes = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        furhat.say("All right, your demands have been noted and will be read by the crew. Let's move on then.")
-        goto(StarshipActivities)
-    }
-}
-
-val NoWishes = state(Interaction) {
-    onEntry {
-        parallel {
-            goto(LookAround)
-        }
-
-        furhat.say("Alright, then let's move on.")
-        goto(StarshipActivities)
-    }
-}*/
-
-
-/*
-fun OrderReceived(fruits: FruitList) : State = state(Options) {
-    onEntry {
-        furhat.say("${fruits.text}, what a lovely choice!")
-        fruits.list.forEach {
-            users.current.order.fruits.list.add(it)
-        }
-        furhat.ask("Anything else?")
-    }
-
-    onReentry {
-        furhat.ask("Did you want something else?")
-    }
-
-    onResponse<No> {
-        furhat.say("Okay, here is your order of ${users.current.order.fruits}. Mission accomplished!")
-    }
-}*/
